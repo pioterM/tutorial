1f703a8e725df228058eb5bb91c76a972c950997
Petr <36698561+pioterM@users.noreply.github.com>
2019-12-19 16:22:58 +0300

    откатил к состоянию без genPart

--- level1.py
+++ level1.py
@@ -5,7 +5,6 @@ import time
 from settings import SIZE, WHITE, HEIGHT, WIDHT, BGCOLOR
 from game_objects import Player, Bullet, EnemyAircraft
 from math import pi, cos, sin
-import genPart
 
 
 def level11(win):
@@ -37,7 +36,6 @@ def level11(win):
     constantShooting = -1
     global player_live
     player_live = 3
-    lastEvent = 0
 
     # ____________________________________
     def game_over():
@@ -57,40 +55,149 @@ def level11(win):
         pygame.quit()
         os.sys.exit()
 
-
     # функция получения названия картинки фона по номеру
-    pygame.mixer.music.load('music.mp3')
-    pygame.mixer.music.play()
 
-    sound1 = pygame.mixer.Sound('music.wav')
+    def bgCadr(i):
+        i = str(i)
+        while len(i) < 4:
+            i = '0' + i
+        return('bg/Видеофон Звёздное небо ' + i+'.jpg')
+
+    global pause
+    pause = False
+
+    def pausedddd():
+        global pause
+        keys = pygame.key.get_pressed()
+        while pause:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    os.sys.exit()
+                if keys[pygame.K_e]:
+                    os.sys.exit()
+            go_font = pygame.font.SysFont('monaco', 72)
+            red = pygame.Color(255, 0, 0)
+            go_surf = go_font.render(' Paused', True, red)
+            go_rect = go_surf.get_rect()
+            go_rect.midtop = (125, 200)
+            play_surface = pygame.display.set_mode((
+                500, 650))
+            pygame.display.set_caption('Shooter')
+            play_surface.blit(go_surf, go_rect.midtop)
+            pygame.display.flip()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_p]:
+                pause = False
+            pygame.display.update()
+
+    # функция обновления экрана
+
+    def drawWindow(win):
+        bg = pygame.image.load(bgCadr(numberbgCadr))
+        win.blit(bg, (0, 0))
+        for playerBullet in playerBullets:
+            win.blit(playerBullet.image, playerBullet.rect)
+        for enemy in enemies:
+            enemyBullets = enemiesBullets[enemies.index(enemy)]
+            for enemyBullet in enemyBullets:
+                win.blit(enemyBullet.image, enemyBullet.rect)
+            if type(enemy) == type(exampleEnemy):
+                win.blit(enemy.image, enemy.rect)
+        win.blit(player.image, player.rect)
+        pygame.display.update()
+
+    # ___________________________________________
+
     while True:
         clock.tick(45)
-        for i in pygame.event.get():
-            if i.type == pygame.QUIT:
-                exit()
-            elif i.type == pygame.KEYUP:
-                if i.key == pygame.K_1:
-                    pygame.mixer.music.pause()
-                    sound1.play()
-                    # pygame.mixer.music.stop()
-                elif i.key == pygame.K_2:
-                    pygame.mixer.music.unpause()
-                    sound1.stop()
-                    # pygame.mixer.music.play()
-                    pygame.mixer.music.set_volume(0.5)
-                elif i.key == pygame.K_3:
-                    pygame.mixer.music.unpause()
-                    # pygame.mixer.music.play()
-                    pygame.mixer.music.set_volume(1)
-            # elif i.type == pygame.MOUSEBUTTONUP:
-            #     if i.button == 1:
-            #         sound1.play()
-            #     elif i.button==2:
-            #         sound1.stop()
-        pygame.time.delay(20)
-        #pygame.mixer.music.load('Test.mp3')
-        #pygame.mixer.music.play()
+        intervalPlayerSnaryad += 1
+        intervalEnemySnaryad += 1
 
+        # счетчик для анимированного фона
+        if numberbgCadr < 7325:
+            numberbgCadr += 1
+        else:
+            numberbgCadr = 0
+
+        # двигаю снаряды врагов и удаляю улетевшие
+        for enemy in enemies:
+            enemyBullets = enemiesBullets[enemies.index(enemy)]
+            for enemyBullet in enemyBullets:
+                if enemyBullet.rect.centery < HEIGHT and enemyBullet.rect.centery >= 0 and type(enemy) == type(exampleEnemy):
+                    enemyBullet.rect.centery += (enemy.vel + enemyBullet.vel) * \
+                        (-round(sin(enemyBullet.angle), 3))
+                    # enemyBullet.rect.bottom += enemyBullet.vel*(-sin(enemy.Bullet.angle*pi/180)) + delta*3
+                    enemyBullet.rect.centerx += (enemy.vel + enemyBullet.vel) * \
+                        (round(cos(enemyBullet.angle), 3))
+                else:
+                    # удалил снаряд с индексом, который вылетел за границы
+                    enemyBullets.pop(enemyBullets.index(enemyBullet))
+
+        # двигаю снаряды игрока и удаляю улетевшие
+        for playerBullet in playerBullets:
+            if playerBullet.rect.centery < HEIGHT and playerBullet.rect.centery > 0:
+                delta = round(random.random())
+                playerBullet.rect.centery -= playerBullet.vel + delta*3
+                playerBullet.rect.bottom -= playerBullet.vel + delta*3
+            else:
+                # удалил снаряд с индексом, который вылетел за границы
+                playerBullets.pop(playerBullets.index(playerBullet))
+
+        # обработка поражения врагов пулями
+        realEnemies = []
+        for enemy in enemies:
+            if type(enemy) == type(exampleEnemy):
+                realEnemies.append(enemy)
+        enemiesGroup = pygame.sprite.Group(realEnemies)
+        playerBulletsGroup = pygame.sprite.Group(playerBullets)
+        hits = pygame.sprite.groupcollide(
+            enemiesGroup, playerBulletsGroup, True, True)
+        for hit in hits:
+            if hit:
+                enemies[enemies.index(hit)] = 0
+        checker = 0
+        for enemy in enemies:
+            if enemy == 0:
+                checker = 1
+
+        # обработка поражения игрока пулями
+        for enemy in enemies:
+            enemyBullets = enemiesBullets[enemies.index(enemy)]
+            enemyBulletsGroup = pygame.sprite.Group(enemyBullets)
+            playerBulletsGroup = pygame.sprite.Group(player)
+            hits = pygame.sprite.groupcollide(
+                enemyBulletsGroup, playerBulletsGroup, True, True)
+            for hit in hits:
+                if hit:
+                    enemyBullets.pop(enemyBullets.index(hit))
+                    # уничтожение пули
+
+                    player_live -= 1
+                    if player_live < 0:
+                        time.sleep(0)
+                        print('Game over')
+                        print(player_live)
+                        game_over()
+
+                    # os.sys.exit(0)
+
+        # обработка некоторых клавиш (выхода и постоянной стрельбы)
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                os.sys.exit()
+            if keys[pygame.K_e]:
+                os.sys.exit(0)
+            if keys[pygame.K_c] and event.type == pygame.KEYUP:
+                constantShooting *= -1
+
+        # движение игрока за стрелкой
+        if event.type == pygame.MOUSEMOTION:
+            [player.rect.centerx, player.rect.centery] = event.pos
+            mouseCoords = event.pos
+        if keys[pygame.K_o]:
+            pause = True
+            pausedddd()
 
         # UPDATE  - -- -- - -- движение врагов
         for enemy in enemies:
@@ -122,17 +229,22 @@ def level11(win):
                                  1].rect.centery = enemy.rect.centery
             enemiesTimerSnaryad = False
 
-        # обработка поражения врагов пулями
-        realEnemies = []
-        for enemy in enemies:
-            if type(enemy) == type(exampleEnemy):
-                realEnemies.append(enemy)
-        enemiesGroup = pygame.sprite.Group(realEnemies)
-        playerBulletsGroup = pygame.sprite.Group(playerBullets)
-        hits = pygame.sprite.groupcollide(
-            enemiesGroup, playerBulletsGroup, True, True)
-        for hit in hits:
-            if hit:
-                enemies[enemies.index(hit)] = 0
-        genPart.generalPart(player, playerBullets, enemies, enemiesBullets, exampleEnemy,
-                            constantShooting, intervalPlayerSnaryad, intervalEnemySnaryad, win, numberbgCadr, playerTimerSnaryad, mouseCoords, lastEvent)
+        # стрельба игрока
+        if keys[pygame.K_f] or event.type == pygame.MOUSEBUTTONDOWN or constantShooting > 0:
+            if playerTimerSnaryad == True:
+                playerBullets.append(Bullet(pi/2, 2, 20))
+                playerBullets[len(
+                    playerBullets) - 1].rect.centerx = mouseCoords[0] - rightleftGun
+                playerBullets[len(playerBullets) -
+                              1].rect.centery = mouseCoords[1]
+                rightleftGun *= -1
+                playerTimerSnaryad = False
+
+        drawWindow(win)
+        # проверка счетчика интервала между пулями
+        if intervalPlayerSnaryad >= 2:
+            playerTimerSnaryad = True
+            intervalPlayerSnaryad = 0
+        if intervalEnemySnaryad >= 60:
+            enemiesTimerSnaryad = True
+            intervalEnemySnaryad = 0
